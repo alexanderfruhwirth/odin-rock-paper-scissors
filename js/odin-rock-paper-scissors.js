49dfbc567182657081d8203c02673866a65dbc44
alexanderfruhwirth <alexander.fruhwirth@gmail.com>
2026-09-01 12:15:36 +0200

    Add pseudocode for computer choice

---
+++ js/odin-rock-paper-scissors.js
@@ -0,0 +1,7 @@
+/* Pseudocode for computer choice
+-- Getting the computer choice of rock, paper or scissors as a return value of a function --
+- When this function is called, initialise a variable with a random number between 0 (inclusive) and 1 (exclusive) as its value
+- If the variable's value is smaller than 1/3 return the string "rock"
+- Else if the variable's value is smaller than 2/3 return the string "paper"
+- Else return the string "scissors"
+*/
